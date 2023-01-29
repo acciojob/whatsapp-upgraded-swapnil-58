cb141e9f7c29f2cc51021bc60537f7b0961ec3e4
swapnil-58 <swapnilmore8983@gmail.com>
2023-01-29 12:27:21 +0530

    final push

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -3,6 +3,11 @@ package com.driver;
 public class Group {
     private String name;
     private int numberOfParticipants;
+
+
+    public Group() {
+    }
+
     public Group(String name, int numberOfParticipants) {
         this.name = name;
         this.numberOfParticipants = numberOfParticipants;
--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -2,25 +2,18 @@ package com.driver;
 
 import java.util.Date;
 
+
 public class Message {
+
     private int id;
     private String content;
     private Date timestamp;
 
-    public Message(int id) {
-        this.id = id;
-        this.timestamp = new Date();
-    }
+
 
     public Message(int id, String content) {
         this.id = id;
         this.content = content;
-        this.timestamp = new Date();
-    }
-    public Message(int id, String content, Date timestamp) {
-        this.id = id;
-        this.content = content;
-        this.timestamp = timestamp;
     }
 
     public int getId() {
